155a3037054b45395719cb16fc66b8004c910aa6
Gillan <gillanropero@gmail.com>
2026-01-20 22:04:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+  $(".faq-answer").hide();
 
+  $(".faq-question").click(function(e) {
+    e.stopPropagation();
+
+    const $answer = $(this).next(".faq-answer");
+
+    $(".faq-question").not(this).removeClass('active');
+    $(".faq-answer").not($answer).slideUp("fast");
+
+    $(this).toggleClass('active');
+    $answer.slideToggle("fast");
+  });
+
+  $(document).click(function(e) {
+    if (!$(e.target).closest('.container').length) {
+      $(".faq-answer").slideUp("fast");
+      $(".faq-question").removeClass('active');
+    }
+  });
